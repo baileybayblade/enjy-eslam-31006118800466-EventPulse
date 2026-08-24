392dc182edf7363ad125d7e1848a8a3d1339ac57
baileybayblade <enjyeslam@gmail.com>
2026-08-24 14:20:44 +0300

    feat/fix: Several registration route changes, mainly by matching all endpoints.

--- routes/registrationRoutes.js
+++ routes/registrationRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const registrationController = require('../controllers/registrationController');
-const { protect } = require('../middleware/authMiddleware');
+const registrationController = require('../controllers/registrations.controller');
+const requireAuth = require('../middleware/requireAuth');
 
 // all registration routes are protected for authenticated users :3
-router.use(protect);
+router.use(requireAuth);
 
 router.post('/', registrationController.registerForEvent);
-router.get('/my-registrations', registrationController.getMyRegistrations);
+router.get('/my', registrationController.getMyRegistrations);
 router.delete('/:id', registrationController.cancelRegistration);
 
 module.exports = router;
