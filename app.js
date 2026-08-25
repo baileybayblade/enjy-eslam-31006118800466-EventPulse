44c2b7c0656916f920b2f69bdce77b394e84efd5
baileybayblade <enjyeslam@gmail.com>
2026-08-25 15:39:42 +0300

    docs: Added a small comment concerning the Swagger addition.

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const SWAGGER_OPTIONS = {
     'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.3/swagger-ui-bundle.js',
     'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.3/swagger-ui-standalone-preset.js',
   ],
-};
+}; // Added this so it doesn't look blank. I know it's not necessary, but I didn't like the pure white page!
 
 // middleware 4 express
 app.use(morgan('dev'));
